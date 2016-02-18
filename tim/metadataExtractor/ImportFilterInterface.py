59a6daa2efbf1c7a35fc680e646e78f43635bb08
christophtrybek <christoph.trybek@gmail.com>
2016-02-18 16:08:23 +0100

    Created meta data filter for the image formats bmp, gif, jpeg, png and tiff
    Mapped all filters to mapping dictionary

--- tim/metadataExtractor/ImportFilterInterface.py
+++ tim/metadataExtractor/ImportFilterInterface.py
@@ -5,30 +5,28 @@ Created on Jan 19, 2016
 '''
 import logging
 
+
 class ImportFilterInterface(object):
-	'''
+    '''
 	classdocs
 	'''
-	log = logging.getLogger() # logger for all import filters
-	
-	def __init__(self):
-		'''
+    log = logging.getLogger()  # logger for all import filters
+
+    def __init__(self):
+        '''
 		Constructor
 		'''
-		
-		
-	def cleanupMetaDataDict(self, mdRaw):
-		mdConv = dict()
-		#print(mdRaw)
-		for k, v in mdRaw.items():
-				k_clean = k.replace(' ', '-').lower()
-				if k_clean in self.myValidTagNames and len(v.__str__())>0:
-					mdConv['x-object-meta-filter-{}-{}'.format(self.myName, k_clean)] = v.__str__()
-		return mdConv	
-		
-	def extractMetaData(self, obj):
-		result = {'placeholder':'placeholder'}
-		print("TEST")
-		return result
-	
-	
+
+    def cleanupMetaDataDict(self, mdRaw):
+        mdConv = dict()
+        # print(mdRaw)
+        for k, v in mdRaw.items():
+            k_clean = k.replace(' ', '-').lower()
+            if k_clean in self.myValidTagNames:
+                mdConv['x-object-meta-filter-{}-{}'.format(self.myName, k_clean)] = v.__str__()
+        return mdConv
+
+    def extractMetaData(self, obj):
+        result = {'placeholder': 'placeholder'}
+        print("TEST")
+        return result
