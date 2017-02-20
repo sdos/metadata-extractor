b94e2ec77039465252ea39a695a817676a97ad4e
Tim Waizenegger <tim.waizenegger@ipvs.uni-stuttgart.de>
2017-02-20 15:52:13 +0100

    new default env

--- mcm/metadataExtractor/configuration.example.py
+++ mcm/metadataExtractor/configuration.example.py
@@ -18,7 +18,7 @@
 	This is the configuration file
 """
 
-import logging, math, os
+import logging, os
 
 
 ###############################################################################
@@ -27,31 +27,62 @@ import logging, math, os
 """
 #log_level = logging.CRITICAL
 #log_level = logging.ERROR
-#log_level = logging.WARNING
+log_level = logging.WARNING
 #log_level = logging.INFO
-log_level = logging.DEBUG
-log_format = '%(asctime)s - %(module)s - %(levelname)s ##\t  %(message)s'
+#log_level = logging.DEBUG
 
 ###############################################################################
-"""
-	API / service settings
-"""
-kafka_broker_endpoint = "172.18.0.33:9092"
 
-swift_store_url_valid_prefix = "http://172.18.0.11:3000/v1/AUTH_"
+"""
+################################################################################
+MCM tenant
+ this extractor is always responsible for one tenant. Use multiple extractor
+ instances for multiple tenants
+################################################################################
+"""
 
-#my_tenant_id = "ea012720129645d9b32b23b4af5c154f"
 my_tenant_id = "mcmdemo"
+
+
+
+"""
+################################################################################
+define the Swift server connection below:
+localhost:3000 is the default for the SDOS API proxy.
+here we assume that auth/store run on the same host/port. this is true with SDOS
+ as well as swift-all-in-one setups.
+ If bluebox is directly connected to Swift (without SDOS proxy) then you may need
+ to configure two different endpoints below
+################################################################################
+"""
+
+swift_host = os.getenv("SWIFT_HOST", "localhost")
+swift_port = os.getenv("SWIFT_PORT", 3000)
+
+swift_store_url_valid_prefix = "http://{}:{}/v1/AUTH_".format(swift_host, swift_port)
+
+"""
+################################################################################
+Kafka bootstrap broker for messaging
+################################################################################
+"""
+kafka_host = os.getenv("KAFKA_HOST", "localhost")
+kafka_port = os.getenv("KAFKA_PORT", 9092)
+
+kafka_broker_endpoint = "{}:{}".format(kafka_host, kafka_port)
 
 """
 ################################################################################
 used by Analytics. Endpoint of the metadata warehouse PostgreSQL db
 ################################################################################
 """
+postgres_host = os.getenv("POSTGRES_HOST", "localhost")
+postgres_port = os.getenv("POSTGRES_PORT", 5432)
+
 metadata_warehouse_endpoint = {
-	"database": "mcm_metadata_{}".format(my_tenant_id),
-	"user": "postgres",
-	"password": "passw0rd",
-	"host": "172.18.0.44",
-	"port": "5432"
+    "database": "mcm_metadata_{}".format(my_tenant_id),
+    "user": "postgres",
+    "password": "passw0rd",
+    "host": postgres_host,
+    "port": postgres_port
 }
